295a6ea4530b7321c59e4bee78e7b1ad81f09f4b
Helmi <helmi.prasetyo12@gmail.com>
2025-04-25 21:11:04 +0700

    Update useDebounce.tsx

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 const useDebounce = () => {
   const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
 
-  const debounce = (func: Function, delay: number) => {
+  const debounce = (func: () => void, delay: number) => {
     if (debounceTimeout.current) clearTimeout(debounceTimeout.current);
     debounceTimeout.current = setTimeout(() => {
       func();
