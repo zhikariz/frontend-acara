9de7b80c6a5ac4b0a015b7d02c5de0f3e1e14ca9
Helmi <helmi.prasetyo12@gmail.com>
2025-04-29 20:16:12 +0700

    Update InputFile.tsx

--- src/components/ui/InputFile/InputFile.tsx
+++ src/components/ui/InputFile/InputFile.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/utils/cn";
 import { Button, Spinner } from "@heroui/react";
 import Image from "next/image";
-import { ChangeEvent, useEffect, useId, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useId, useRef } from "react";
 import { CiSaveUp2, CiTrash } from "react-icons/ci";
 
 interface PropTypes {
